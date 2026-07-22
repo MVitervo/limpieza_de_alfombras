a51eba2f3bc9d525f4ce93533a83098609938f24
mvitervoIMSSA <miguel.vitervo@imssa1.com>
2026-07-22 09:03:15 -0600

    se aplico diseño a los campos

--- index.php
+++ index.php
@@ -9,13 +9,13 @@
 
 </head>
 
-<body class="bg-white text-black dark:bg-gray-900 dark:text-white transition-colors duration-300">
+<body class="bg-olive-100 text-black dark:bg-gray-900 dark:text-white transition-colors duration-300">
 
+    
     <!-- Cambio de tema -->
     <button id="toggle-theme" class="px-2 py-2 bg-gray-200 dark:bg-gray-700 text-gray-800 dark:text-gray-200 rounded transition-colors duration-300 mt-4 ml-4">
         🌙
     </button>
-
     <form class="px-4">
         <div class="w-full md:w-1/2 mx-auto">
             <div class="grid grid-cols-2 gap-6">
@@ -23,11 +23,8 @@
                     <label for="first_name" class="block mb-2.5 text-sm font-medium text-heading text-gray-700 dark:text-gray-200">First name</label>
                     <input
                         class="
-                        w-full
-                        px-3
-                        py-2.5
-                        rounded-base
-                        border
+                        w-full rounded-lg border border-gray-300 px-4 py-2 text-gray-900 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500
+
                         bg-white
                         text-black
                         border-gray-300
@@ -43,11 +40,9 @@
                     <label for="last_name" class="block mb-2.5 text-sm font-medium text-heading text-gray-700 dark:text-gray-200">Last name</label>
                     <input
                         class="
-                        w-full
-                        px-3
-                        py-2.5
-                        rounded-base
-                        border
+                        w-full rounded-lg border border-gray-300 px-4 py-2 text-gray-900 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500
+
+
                         bg-white
                         text-black
                         border-gray-300
@@ -62,7 +57,16 @@
 
             <div>
                 <label for="last_name" class="block mb-2.5 text-sm font-medium text-heading">Last name</label>
-                <input type="text" id="last_name" class="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body" placeholder="John" required />
+                <input type="text" id="last_name" class="w-full rounded-lg border border-gray-300 px-4 py-2 text-gray-900 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500
+                        
+                        bg-white
+                        text-black
+                        border-gray-300
+
+                        dark:bg-gray-800
+                        dark:text-white
+                        dark:border-gray-600
+                        dark:placeholder-gray-400" placeholder="John" required />
             </div>
         </div>
     </form>
