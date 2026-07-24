807f49809ed6ba5f1150f1a3e5d0713827a251d3
Miguel Vitervo <Vitervo_2000@outlook.com>
2026-07-23 23:36:20 -0600

    se agrego el calendario

--- views/information.php
+++ views/information.php
@@ -1,6 +1,6 @@
 <form class="px-4">
     <div class="w-full md:w-1/2 mx-auto">
-        <div class="grid grid-cols-2 gap-6">
+        <div class="grid grid-cols-2 gap-6 mb-3">
             <div>
                 <label for="first_name" class="block mb-2.5 text-sm font-medium text-heading text-gray-700 dark:text-gray-200">First name</label>
                 <input
@@ -30,14 +30,32 @@
             </div>
         </div>
 
-        <div>
-            <label for="last_name" class="block mb-2.5 text-sm font-medium text-heading">Last name</label>
-            <input type="text" id="last_name" class="w-full rounded-lg border border-gray-300 px-4 py-2 text-gray-900 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500
-                        bg-white
-                        dark:bg-gray-800
-                        dark:text-white
-                        dark:border-gray-600
-                        dark:placeholder-gray-400" placeholder="John" required />
+
+        <div class="grid grid-cols-2 gap-6">
+            <div>
+                <label for="date" class="block mb-2.5 text-sm font-medium text-heading text-gray-700 dark:text-gray-200">Select date</label>
+                <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                    <svg
+                        class="w-5 h-5 text-gray-400 dark:text-gray-500"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor">
+
+                        <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            d="M4 10h16M8 4v3m8-3v3M5 20h14a1 1 0 001-1V7a1 1 0 00-1-1H5a1 1 0 00-1 1v12a1 1 0 001 1zm3-7h.01M12 13h.01M16 13h.01M8 17h.01M12 17h.01M16 17h.01" />
+                    </svg>
+                </div>
+
+                <input
+                    type="date"
+                    class="block w-full rounded-lg border border-gray-300 bg-white py-2.5 pl-10 pr-3 text-sm text-gray-900 shadow-sm transition-colors placeholder-gray-400 focus:border-blue-500 focus:ring-2 focus:ring-blue-500 focus:outline-none dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-400 dark:focus:ring-blue-400"
+                    placeholder="Selecciona una fecha">
+            </div>
         </div>
+
     </div>
 </form>
