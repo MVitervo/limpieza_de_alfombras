1ea24fb7afa33595d73993f7d8a802f48f4a678d
Miguel Vitervo <Vitervo_2000@outlook.com>
2026-07-28 20:56:07 -0600

    archivo faltante

--- controllers/consult_schedules_controller.php
+++ controllers/consult_schedules_controller.php
@@ -1,6 +1,11 @@
 <?php 
 
 header('Content-Type: application/json');
-require_once __DIR__ . '/../database/connection.php';
+// require_once __DIR__ . '/../database/connection.php';
+require_once './services/consult_schedule.php';
+
+function loadSchedules() {
+    
+}
 
 ?>
