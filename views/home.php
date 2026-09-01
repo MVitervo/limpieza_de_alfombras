20b7e15964e36be395314e2700a10535889b17a1
Miguel Vitervo <Vitervo_2000@outlook.com>
2026-08-31 22:52:49 -0600

    ajuste del select2 asi como el diseño en modo ocuro y la implementacion de los dialog de tailwind para los mensajes de error y los mensajes en general

--- views/home.php
+++ views/home.php
@@ -36,6 +36,7 @@
     <script src="/public/js/bootstrap.bundle.min.js"></script>
     <script src="/public/js/select2.min.js"></script>
 
+
 </head>
 <!-- continuar con el modo oscuro del calendario y trabajar con el calendario -->
 
@@ -53,6 +54,7 @@
     <main>
 
         <?php require __DIR__ . '/information.php'; ?>
+        <?php require __DIR__ . '/modal_error.php'; ?>
 
     </main>
 
--- views/information.php
+++ views/information.php
@@ -1,6 +1,6 @@
 <form class="px-4" id="appointmentForm">
     <div class="w-full md:w-1/2 mx-auto">
-        <div class="grid grid-cols-2 gap-6 mb-3">
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-6 mb-3">
             <div>
                 <label for="first_name" class="block mb-2.5 text-sm font-medium text-heading text-gray-700 dark:text-gray-200">First name</label>
                 <input
@@ -30,7 +30,7 @@
             </div>
         </div>
 
-        <div class="grid grid-cols-2 gap-6 mb-3">
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-6 mb-3">
             <div>
                 <div class="mb-4">
                     <label for="email"
@@ -68,7 +68,7 @@
             </div>
         </div>
 
-        <div class="grid grid-cols-2 gap-6 mb-3">
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-6 mb-3">
             <div>
                 <div class="mb-4">
                     <label for="expectDate"
@@ -121,45 +121,21 @@
             <div id="calendar"></div>
         </div> -->
 
-        <button type="submit">Guardar</button>
+        <button type="submit" 
+        class="w-full py-2 px-4
+        bg-white-500/10 text-black
+        dark:bg-white-500/10 dark:text-white font-semibold border border-gray-400 rounded shadow
+        ">
+            Guardar
+        </button>
+
+        <!-- <div class="bg-sky-500/10"></div> -->
+
     </div>
 </form>
 
-<!-- Include this script tag or install `@tailwindplus/elements` via npm: -->
-<!-- <script src="https://cdn.jsdelivr.net/npm/@tailwindplus/elements@1" type="module"></script> -->
-<button command="show-modal" commandfor="dialog" class="rounded-md bg-gray-950/5 dark:bg-gray-700 px-2.5 py-1.5 text-sm font-semibold text-gray-900 dark:text-gray-200 hover:bg-gray-950/10">Open dialog</button>
 
-<!-- px-2 py-2 bg-gray-200 dark:bg-gray-700 text-gray-800 dark:text-gray-200 rounded transition-colors duration-300 mt-4 ml-4 -->
-
-<el-dialog>
-    <dialog id="dialog" aria-labelledby="dialog-title" class="fixed inset-0 size-auto max-h-none max-w-none overflow-y-auto bg-transparent backdrop:bg-transparent">
-        <el-dialog-backdrop class="fixed inset-0 bg-gray-500/75 transition-opacity data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in"></el-dialog-backdrop>
-
-        <div tabindex="0" class="flex min-h-full items-end justify-center p-4 text-center focus:outline-none sm:items-center sm:p-0">
-            <el-dialog-panel class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all data-closed:translate-y-4 data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in sm:my-8 sm:w-full sm:max-w-lg data-closed:sm:translate-y-0 data-closed:sm:scale-95">
-                <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-                    <div class="sm:flex sm:items-start">
-                        <div class="mx-auto flex size-12 shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:size-10">
-                            <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" data-slot="icon" aria-hidden="true" class="size-6 text-red-600">
-                                <path d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126ZM12 15.75h.007v.008H12v-.008Z" stroke-linecap="round" stroke-linejoin="round" />
-                            </svg>
-                        </div>
-                        <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-                            <h3 id="dialog-title" class="text-base font-semibold text-gray-900">Deactivate account</h3>
-                            <div class="mt-2">
-                                <p class="text-sm text-gray-500">Are you sure you want to deactivate your account? All of your data will be permanently removed. This action cannot be undone.</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-                    <button type="button" command="close" commandfor="dialog" class="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-red-500 sm:ml-3 sm:w-auto">Deactivate</button>
-                    <button type="button" command="close" commandfor="dialog" class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-xs inset-ring inset-ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto">Cancel</button>
-                </div>
-            </el-dialog-panel>
-        </div>
-    </dialog>
-</el-dialog>
+<button class="dialogErrorDatabaseButton" command="show-modal" commandfor="dialogErrorDatabase"></button>
 
 
 <script>
@@ -169,6 +145,7 @@
         $('.expectHour').select2({
             theme: 'bootstrap-5',
             placeholder: 'first select a date',
+            width: '100%'
         });
         // loadSchedules();
     });
@@ -213,6 +190,7 @@
             },
             error: function(response) {
                 debugger;
+                document.querySelector('.dialogErrorDatabaseButton').click();
             }
         });
     }
